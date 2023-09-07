43cea833a75f9283fc80560da94cb89243eb3505
1715819 <75056973+1715819@users.noreply.github.com>
2023-09-07 18:26:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/tg_idsbot.git /tg_idsbot
+git clone https://github.com/1715819/tg_idsbot /tg_idsbot
 cd /tg_idsbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 bot.py &
+python3 app.py &
