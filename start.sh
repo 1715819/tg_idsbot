3a441c092290d61f5f00e2760a02f0b16158ffcc
1715819 <75056973+1715819@users.noreply.github.com>
2023-09-07 18:55:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@ git clone https://github.com/1715819/tg_idsbot.git /tg_idsbot
 cd /tg_idsbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py &
-python3 app.py &
+python3 bot.py
