6dadd8f65fbe7084b77fd358f0b1391cf89c5a38
1715819 <75056973+1715819@users.noreply.github.com>
2023-09-07 20:54:41 +0800

    Update start.py

--- tg_idsbot/start.py
+++ tg_idsbot/start.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python3
-# Copyright (C) @ZauteKm
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from data import Data
 from config import Config
 from pyrogram import Client, filters
@@ -23,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup
 async def start(bot, msg):
     print("/start")
     user = await bot.get_me()
-    mention = user["mention"]
+    mention = getattr(user, "mention", None)
     await bot.send_message(
         msg.chat.id,
         Data.START.format(msg.from_user.mention, mention, msg.from_user.id),
